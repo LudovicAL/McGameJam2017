50b68b7b3bf6af56d30a9e27c8b566f2656faf07
Ludovic <ludovicaubut@hotmail.com>
2017-03-06 16:57:06 -0500

    Just started coding a PlayerManager as it seem we may have access to a XBox Controller.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,9 @@
+﻿public class Player {
+	public string name {get; private set;}
+	public Controller controller {get; private set;}
+
+	public Player(string name, Controller controller) {
+		this.name = name;
+		this.controller = controller;
+	}
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+	private List<Controller> listOfAvailableContollers;
+	public List<Player> listOfPlayers;
+
+	// Use this for initialization
+	void Start () {
+		listOfPlayers = new List<Player> ();
+		listOfAvailableContollers = new List<Controller>() {
+			new Controller("KL"),	//Keyboard Left
+			new Controller("KR"),	//Keyboard Right
+			new Controller("C1"),	//Controller 1
+			new Controller("C2"),	//Controller 2
+			new Controller("C3"),	//Controller 3
+			new Controller("C4"),	//Controller 4
+			new Controller("C5"),	//Controller 5
+			new Controller("C6"),	//Controller 6
+			new Controller("C7"),	//Controller 7
+			new Controller("C8"),	//Controller 8
+			new Controller("C9"),	//Controller 9
+			new Controller("C10"),	//Controller 10
+			new Controller("C11")	//Controller 11
+		};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (Controller controller in listOfAvailableContollers) {
+			if (Input.GetButton(controller.buttonA)) {
+				AddPlayer (controller);
+				break;
+			}
+		}
+		foreach (Player player in listOfPlayers) {
+			if (Input.GetButton(player.controller.buttonB)) {
+				RemovePlayer (player);
+				break;
+			}
+		}
+	}
+
+	private void AddPlayer(Controller cm) {
+
+	}
+
+	private void RemovePlayer(Player p) {
+
+	}
+}
