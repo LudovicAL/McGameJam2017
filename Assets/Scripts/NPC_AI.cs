dc726a58bc29dc67485e9e2f13be9d982436d566
Pandaromin <earlaromin@gmail.com>
2017-03-10 23:01:02 -0500

    NPC_AI - WIP
    will continue tomorrow

---
+++ Assets/Scripts/NPC_AI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_AI : MonoBehaviour {
+
+	// Use this for initialization
+	Transform currentNPC;
+	bool isAlerted;
+	float fireDetetectRadius;
+	float hearingRadius;
+	float screamingRadius;
+
+	Transform t;
+	void Start () {
+		isAlerted = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SeekExit()
+	{
+		
+	}
+
+	void Scream()
+	{
+		//emit particles going outward from t
+	}
+		
+}
