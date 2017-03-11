822447c845ce6706c886c661f28f31b0777a04c4
Ludovic <ludovicaubut@hotmail.com>
2017-03-11 05:18:02 -0500

    FireSpreader and FireManager

--- Assets/Scripts/FireSpreader.cs
+++ Assets/Scripts/FireSpreader.cs
@@ -1,15 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class FireSpreader : MonoBehaviour {
 
+	public GameObject firePrefab;
 	public float chanceOfSpreadingToAdjacentTile; //A number between 0 and 1
 	public float chanceOfSpreadingToNPC; //A number between 0 and 1
 	public float spreadingDelay;	//A number superior to 0
+	public float maxBurningTime;
+	private float endTime;
 	private float timeSinceLastSpread;
+	private FireManager fireManager;
+	private GraphNode currentNode;
+	private List<GraphNode> intactNeighbors;
+	private AstarPath graphEngine;
 
 	void Start(){
+		fireManager = GameObject.Find ("ScriptsBucket").GetComponent<FireManager> ();
+		currentNode = AstarPath.active.GetNearest (transform.position).node;
+		intactNeighbors = GetIntactNeighbors ();
+		endTime = Time.time + maxBurningTime;
 		timeSinceLastSpread = 0.0f;
 		chanceOfSpreadingToAdjacentTile = Mathf.Clamp (chanceOfSpreadingToAdjacentTile, 0.0f, 1.0f);
 		chanceOfSpreadingToNPC = Mathf.Clamp (chanceOfSpreadingToAdjacentTile, 0.0f, 1.0f);
@@ -18,23 +30,61 @@ public class FireSpreader : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (HasIntactAdjacentTiles()) {
+		if (Time.time > endTime || intactNeighbors.Count == 0){
+			Destroy (this);
+		} else {
 			timeSinceLastSpread += Time.deltaTime;
 			if (timeSinceLastSpread < spreadingDelay) {
-				SpreadToAdjacentTile ();
+				SpreadToIntactNeighbor ();
 				timeSinceLastSpread = 0.0f;
 			}
 		}
 	}
 
-	private void SpreadToAdjacentTile(){
-		float rnd = Random.Range (0.0f, 1.0f);
-		if (rnd <= chanceOfSpreadingToAdjacentTile) {	//Fire is spreading!
-
+	private void SpreadToIntactNeighbor(){
+		float rndF = Random.Range (0.0f, 1.0f);
+		if (rndF <= chanceOfSpreadingToAdjacentTile) {	//Fire is spreading!
+			int rndI = Random.Range(0, intactNeighbors.Count - 1);
+			GameObject.Instantiate (firePrefab, (Vector3)intactNeighbors [rndI].position, Quaternion.identity);
+			fireManager.AddBurningNode (intactNeighbors [rndI]);
+			intactNeighbors.RemoveAt (rndI);
 		}
 	}
 
-	private bool HasIntactAdjacentTiles() {
-		return false;
+	private List<GraphNode> GetIntactNeighbors() {
+		List<GraphNode> listOfNodes = new List<GraphNode> ();
+		NNConstraint contrainte = new NNConstraint();
+		contrainte.constrainDistance = true;
+		contrainte.constrainWalkability = true;
+		contrainte.walkable = true;
+		AstarPath graphEngine = AstarPath.active;
+		graphEngine.maxNearestNodeDistance = 0.0f;
+		NNInfo info = graphEngine.graphs[0].GetNearestForce (transform.position + Vector3.right, contrainte);
+		if (info.node != null
+		    && !fireManager.burningNodes.Contains(info.node)
+		    && !fireManager.burntNodes.Contains(info.node)) {
+			listOfNodes.Add (info.node);
+		}
+		info = graphEngine.graphs[0].GetNearestForce (transform.position + Vector3.left, contrainte);
+		if (info.node != null
+		    && !fireManager.burningNodes.Contains(info.node)
+		    && !fireManager.burntNodes.Contains(info.node)) {
+			listOfNodes.Add (info.node);
+		}
+		info = graphEngine.graphs[0].GetNearestForce (transform.position + Vector3.forward, contrainte);
+		if (info.node != null
+		    && !fireManager.burningNodes.Contains(info.node)
+		    && !fireManager.burntNodes.Contains(info.node)) {
+			listOfNodes.Add (info.node);
+		}
+		info = graphEngine.graphs[0].GetNearestForce (transform.position + Vector3.back, contrainte);
+		if (info.node != null
+		    && !fireManager.burningNodes.Contains(info.node)
+		    && !fireManager.burntNodes.Contains(info.node)) {
+			listOfNodes.Add (info.node);
+		}
+		return listOfNodes;
 	}
+
+
 }
---
+++ Assets/Scripts/LudosPathfinding/AStarPathfinding.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class AStarPathfinding {
+
+	//Generates a path to a location (A Star Algorithm, I didn't invent this)
+	public static List<Tile> GeneratePath(Tile currentTile, Tile targetTile, GameObject grid) {
+		//Verify that destination is walkable
+		if (targetTile.tileType == Tile.AvailableTileTypes.Wall) {
+			return null;
+		}
+		//Initialize variables
+		Dictionary<int, int> distance = new Dictionary<int, int>();
+		Dictionary<int, int> previousTile = new Dictionary<int, int>();
+		List<int> unvisited = new List<int>(); //A list of nodes that haven't been visited yet
+		int currentTileIndex = grid.GetComponent<GridManager>().walkableTileList.IndexOf(currentTile);
+		distance[currentTileIndex] = 0;	//Reference the source tile distance to itself in the dictionnaries
+		previousTile[currentTileIndex] = -1;	//Reference the source tile previous tile in the dictionnaries
+		//Initialize every tile to have INFINITY distance
+		foreach(Tile t in grid.GetComponent<GridManager>().walkableTileList) {
+			int tIndex = grid.GetComponent<GridManager> ().walkableTileList.IndexOf (t);
+			if(t != currentTile) {
+				distance[tIndex] = int.MaxValue;
+				previousTile[tIndex] = -1;
+			}
+			unvisited.Add(tIndex);
+		}
+		//Visit every tile
+		while(unvisited.Count > 0) {
+			Tile u = null; //"u" is going to be the unvisited node with the smallest distance.
+			int uIndex = 0;
+			foreach(int possibleU in unvisited) {
+				if(u == null || distance[possibleU] < distance[grid.GetComponent<GridManager>().walkableTileList.IndexOf(u)]) {
+					u = grid.GetComponent<GridManager>().walkableTileList.ElementAt(possibleU);
+					uIndex = possibleU;
+				}
+			}
+			if(u == targetTile) {
+				break;
+			}
+			unvisited.Remove(uIndex);
+			foreach(Tile t in u.walkableNeighborTiles) {
+				int alt = distance[uIndex] + 1;
+				int vTileIndex = grid.GetComponent<GridManager> ().walkableTileList.IndexOf (t);
+				if(alt < distance[vTileIndex]) {
+					distance[vTileIndex] = alt;
+					previousTile[vTileIndex] = uIndex;
+				}
+			}
+		}
+		//Getting here means we found the shortest route or there is no route to our target
+		if(previousTile[grid.GetComponent<GridManager>().walkableTileList.IndexOf(targetTile)] == -1) {	//If no route between our target and the source
+			return null;
+		}
+		Tile curr = targetTile;
+		List<Tile> path = new List<Tile> ();
+		//Step through the "prev" chain and add it to our path
+		while(curr != null) {
+			path.Add(curr);
+			int currIndex = grid.GetComponent<GridManager> ().walkableTileList.IndexOf (curr);
+			if (previousTile[currIndex] != -1) {
+				curr = grid.GetComponent<GridManager>().walkableTileList.ElementAt(previousTile[currIndex]);
+			} else {
+				curr = null;
+			}
+		}
+		//Invert the path as it is currently stored backward
+		path.Reverse();
+		return path;
+	}
+}
