a6782bb667b945f7e328b06f30662a314018bb57
Pandaromin <earlaromin@gmail.com>
2017-03-12 05:42:21 -0400

    SprinklerBaby-Genesis

---
+++ Assets/Scripts/Sprinkler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class Sprinkler : MonoBehaviour {
+
+	// Use this for initialization
+	private GameObject sprinkler;
+
+	GameObject partSys;
+
+	void Start () {
+		sprinkler = this.gameObject;
+		partSys =  sprinkler.transform.Find("Particle System").gameObject.GetComponent<Transform>().gameObject;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void ExtinguishFire()
+	{
+		
+	}
+}
