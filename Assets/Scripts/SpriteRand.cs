dc09b745bdcfc0657f761c38de8c5c1655030907
Pandaromin <earlaromin@gmail.com>
2017-03-11 21:46:44 -0500

    Renamed some arms into sleeves

---
+++ Assets/Scripts/SpriteRand.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class SpriteRand : MonoBehaviour {
+
+	// Use this for initialization
+
+	List<Sprite> bodyList = new List<Sprite>();
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
