b55c79bb9bb594275dc23957214eb33b8eb7900a
Gabriel Aubut-Lussier <dalzhim@hotmail.com>
2017-03-12 14:42:19 -0400

    Scaling des assets afin que les éléments soient bien proportionnés.

--- Assets/Scripts/Startup.cs
+++ Assets/Scripts/Startup.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 using Pathfinding;
 public class Startup : MonoBehaviour {
 
+	public GameObject prefabPerso;
+
 	// Use this for initialization
 	void Start () {
 		GameObject dangers = GameObject.Find ("Dangers");
@@ -15,6 +17,21 @@ public class Startup : MonoBehaviour {
 				GraphNode node = NodeUtilities.DonneNodeAvecPosition (danger.position);
 				node.Tag |= StaticData.BURNING_GROUND;
 			}
+		}
+
+		GridGraph grid = AstarPath.active.astarData.gridGraph;
+		List<GraphNode> graphNodes = new List<GraphNode> ();
+		grid.GetNodes(delegate(GraphNode node) {
+			if (node.Walkable) {
+				graphNodes.Add(node);
+			}
+			return true;
+		});
+		int nbPersonnages = Mathf.CeilToInt(graphNodes.Count / (20 * grid.nodeSize));
+		for (int i = 0; i < nbPersonnages; ++i) {
+			int nodeIndex = Random.Range (0, graphNodes.Count);
+			GraphNode node = graphNodes [nodeIndex];
+			GameObject obj = GameObject.Instantiate (prefabPerso, (Vector3)node.position, Quaternion.LookRotation(Vector3.forward));
 		}
 	}
 }
